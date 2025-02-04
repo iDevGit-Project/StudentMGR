5349a6facc9b4b37319c95cb6eaa69a76bc14067
Hamed Shariati Mehr <nextlab.info@gmail.com>
2025-02-04 17:22:01 +0330

    changelog(0.0.4): 14031116: Create Project PATCH0.0.4

---
+++ AttendanceManagement.Service/StudentService/Query/StudentServiceQuery.cs
@@ -0,0 +1,21 @@
+﻿using AttendanceManagement.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceManagement.Service.StudentService.Query
+{
+    public class StudentServiceQuery
+    {
+        #region متد های پیکربندی اطلاعات دانش آموز در سمت سرور 
+
+        private readonly ApplicationDbContext _context;
+        public StudentServiceQuery(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        #endregion
+    }
+}
